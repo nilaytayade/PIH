bb7526fb50d7f07878da8c4b33691f919d5c8187
nilaytayade <136469073+nilaytayade@users.noreply.github.com>
2023-11-08 23:42:10 +0530

    Delete pih23 directory

--- pih23/pih23/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),  # Include the app's URLs
-    path('',home),
-]
--- pih23/pih23/views.py
+++
@@ -1,6 +0,0 @@
-from django import views
-from rest_framework.response import Response
-from django.http import HttpResponse
-
-def home (request):
-    return HttpResponse("HOME")
